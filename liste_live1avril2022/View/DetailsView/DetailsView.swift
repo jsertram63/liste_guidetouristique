895e0c0181309c996c04f29a0bc58e5bae652dd8
PierricM380 <pierric.marye@wanadoo.fr>
2022-04-16 18:36:32 +0200

    Initialisation d'une vue détails des lieux

---
+++ liste_live1avril2022/View/DetailsView/DetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailsView.swift
+//  liste_live1avril2022
+//
+//  Created by Pierric Marye on 16/04/2022.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailsView()
+    }
+}
