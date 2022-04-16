58272fbc2584af9243f321b117defa29b67d5f33
PierricM380 <pierric.marye@wanadoo.fr>
2022-04-16 19:03:16 +0200

    Construction de la vue pour ensuite passer les informations du model

--- liste_live1avril2022/View/DetailsView/DetailsView.swift
+++ liste_live1avril2022/View/DetailsView/DetailsView.swift
@@ -6,10 +6,44 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct DetailsView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 48.6359541, longitude: -1.51146),
+        span: MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
+    )
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Map(coordinateRegion: $region)
+                .frame(height: 300)
+            
+            Image("montSaintMichel")
+                .resizable()
+                .frame(width: 150.0, height: 150.0)
+                .clipShape(Circle())
+                .overlay {
+                    Circle().stroke(.white, lineWidth: 4)
+                }
+                .shadow(radius: 7)
+                .offset(y: -85)
+                .padding(.bottom, -75)
+            
+            HStack(alignment: .center) {
+                Text("Mont Saint Michel")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Text("Wikipedia")
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .background(.brown.opacity(0.5))
     }
 }
 
